1257bade637a53ed45be894a5915144276a02289
manudus <dusautoir@gmail.com>
2026-06-01 10:25:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
-alert("APP.JS CHARGÉ");
+alert("app.js chargé");
+
+const PASSWORD = "6626";
 
 function login() {
 
-alert("LOGIN OK");
+alert("login exécuté");
+
+const pwd =
+document
+.getElementById("pwd")
+.value
+.trim();
+
+if (pwd === PASSWORD) {
+
+document
+.getElementById(
+"loginBox"
+)
+.classList
+.add(
+"hidden"
+);
+
+document
+.getElementById(
+"uploadBox"
+)
+.classList
+.remove(
+"hidden"
+);
+
+} else {
+
+document
+.getElementById(
+"error"
+)
+.textContent =
+"Mot de passe incorrect";
+
+}
 
 }
 ```
