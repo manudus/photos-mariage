ac9a505d3706a85422ce8d04ae2903b0d6e5e1b3
manudus <dusautoir@gmail.com>
2026-06-01 10:34:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-alert("JS CHARGE OK");
-
-console.log("app.js fonctionne");
+const PASSWORD = "6626";
 
 function login() {
-  alert("LOGIN OK");
+
+alert("LOGIN OK");
+
+const pwd =
+document
+.getElementById("pwd")
+.value
+.trim();
+
+if (pwd === PASSWORD) {
+
+document
+.getElementById("loginBox")
+.style.display =
+"none";
+
+document
+.getElementById("uploadBox")
+.style.display =
+"block";
+
+} else {
+
+document
+.getElementById("error")
+.textContent =
+"Mot de passe incorrect";
+
 }
+
+}
+
+/* 🔥 rend la fonction accessible globalement */
+window.login = login;
