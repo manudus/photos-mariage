eed569b152dc597b2e4e72e279d544bb96fcbef2
manudus <dusautoir@gmail.com>
2026-06-01 12:55:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/TON_SCRIPT_ID/exec";
+const SCRIPT_URL = "TON_URL_APPS_SCRIPT";
 
 async function uploadPhotos() {
   const files = document.getElementById("fileInput").files;
   const status = document.getElementById("status");
-
-  const bar = document.getElementById("progressBar");
-  const container = document.getElementById("progressContainer");
 
   if (!files.length) {
     status.textContent = "Ajoute des photos 📸";
     return;
   }
 
-  container.classList.remove("hidden");
-  bar.style.width = "0%";
-
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
+  for (let file of files) {
     const base64 = await toBase64(file);
 
     await fetch(SCRIPT_URL, {
@@ -30,23 +23,16 @@ async function uploadPhotos() {
         code: "6626"
       })
     });
-
-    bar.style.width = Math.round(((i + 1) / files.length) * 100) + "%";
   }
 
   status.textContent = "💛 Photos envoyées !";
-
-  setTimeout(() => {
-    container.classList.add("hidden");
-    document.getElementById("fileInput").value = "";
-  }, 1000);
 }
 
 function toBase64(file) {
-  return new Promise((resolve, reject) => {
+  return new Promise((res, rej) => {
     const reader = new FileReader();
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = reject;
+    reader.onload = () => res(reader.result);
+    reader.onerror = rej;
     reader.readAsDataURL(file);
   });
 }
